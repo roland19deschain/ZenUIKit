1f6ba977ef09b9d054ca1ded0407c504533ae348
Alexey Roik <alexey.roik@nebulagames.org>
2020-04-01 01:34:08 +0300

    Add empty 'Swift' project

---
+++ ZenUIKit/ZenUIKit.h
@@ -0,0 +1,19 @@
+//
+//  ZenUIKit.h
+//  ZenUIKit
+//
+//  Created by Alexey Roik on 4/1/20.
+//  Copyright © 2020 Nebula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZenUIKit.
+FOUNDATION_EXPORT double ZenUIKitVersionNumber;
+
+//! Project version string for ZenUIKit.
+FOUNDATION_EXPORT const unsigned char ZenUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZenUIKit/PublicHeader.h>
+
+
