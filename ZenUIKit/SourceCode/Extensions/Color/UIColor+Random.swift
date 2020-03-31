6d98f1668c65c86ed3c75757a11b4722ff336f14
Alexey Roik <alexey.roik@nebulagames.org>
2020-04-01 01:46:29 +0300

    Add 'UIColor+Random' extension

---
+++ ZenUIKit/SourceCode/Extensions/Color/UIColor+Random.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+extension UIColor {
+	
+	/// Returns an opaque color (alpha 1) with random RGB values.
+	static var opaqueRandom: UIColor {
+		UIColor(
+			red: .random(in: 0...1),
+			green: .random(in: 0...1),
+			blue: .random(in: 0...1),
+			alpha: 1
+		)
+	}
+	
+	/// Return a color with random RGB and alpha values.
+	static var transparenteRandom: UIColor {
+		UIColor(
+			red: .random(in: 0...1),
+			green: .random(in: 0...1),
+			blue: .random(in: 0...1),
+			alpha: .random(in: 0...1)
+		)
+	}
+	
+}
