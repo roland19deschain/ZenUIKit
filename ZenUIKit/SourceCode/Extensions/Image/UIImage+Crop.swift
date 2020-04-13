f77dc29e5c9a4b8d85415c42ece60eb1b9a33f5b
Alexey Roik <alexey.roik@nebulagames.org>
2020-04-14 01:58:49 +0300

    Add 'UIImage' extensions

---
+++ ZenUIKit/SourceCode/Extensions/Image/UIImage+Crop.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+public extension UIImage {
+	
+	/**
+	Creates a bitmap image using the data contained within a subregion of an existing bitmap image.
+	- parameter rect: A rectangle specifying the portion of the image to keep. If the rect parameter defines an area that is not in the image, returns original image.
+	- returns: A subimage of the image.
+	*/
+	func cropped(to rect: CGRect) -> UIImage {
+		guard let cgImage = cgImage,
+			let image = cgImage.cropping(to: rect) else {
+				return self
+		}
+		return UIImage(
+			cgImage: image,
+			scale: scale,
+			orientation: imageOrientation
+		)
+	}
+	
+}
