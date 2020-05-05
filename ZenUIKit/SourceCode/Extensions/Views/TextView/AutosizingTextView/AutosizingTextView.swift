a1c6c535f05d166fe42c89bc5f0a7021ac87309a
Alexey Roik <alexey.roik@nebulagames.org>
2020-05-06 02:16:07 +0300

    Add 'AutosizingTextView' and delegate protocol for it

---
+++ ZenUIKit/SourceCode/Extensions/Views/TextView/AutosizingTextView/AutosizingTextView.swift
@@ -0,0 +1,110 @@
+import UIKit
+
+public final class AutosizingTextView: UITextView {
+	
+	// MARK: - Computed Properties
+	
+	public override var intrinsicContentSize: CGSize {
+		guard maximumAllowableHeight > 0,
+			contentSize.height > maximumAllowableHeight else {
+			return super.intrinsicContentSize
+		}
+		return CGSize(
+			width: UIView.noIntrinsicMetric,
+			height: maximumAllowableHeight
+		)
+	}
+	
+	// MARK: - Stored Properties
+	
+	public weak var autosizingDelegate: AutosizingTextViewDelegate?
+	
+	public var maximumAllowableHeight: CGFloat = 0 {
+		didSet {
+			updateScrollAvailability()
+		}
+	}
+	
+	public override var isScrollEnabled: Bool {
+		didSet {
+			updateLayout()
+		}
+	}
+	
+	// MARK: - Life Cycle
+	
+	public override init(
+		frame: CGRect,
+		textContainer: NSTextContainer? = nil
+	) {
+		super.init(
+			frame: frame,
+			textContainer: textContainer
+		)
+		
+		setupObservers()
+	}
+	
+	public required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		
+		setupObservers()
+	}
+	
+	deinit {
+		NotificationCenter.default.removeObserver(self)
+	}
+	
+}
+
+// MARK: - Layout
+
+extension AutosizingTextView {
+	
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		updateScrollAvailability()
+	}
+	
+}
+
+// MARK: - Update
+
+private extension AutosizingTextView {
+	
+	func updateScrollAvailability() {
+		guard maximumAllowableHeight > 0 else {
+			return
+		}
+		isScrollEnabled = contentSize.height > maximumAllowableHeight
+	}
+	
+	func updateLayout() {
+		invalidateIntrinsicContentSize()
+		
+		autosizingDelegate?.autosizingTextViewDidUpdateLayout(
+			textView: self
+		)
+	}
+	
+}
+
+// MARK: - Observers
+
+private extension AutosizingTextView {
+	
+	func setupObservers() {
+		NotificationCenter.default.addObserver(
+				self,
+				selector: #selector(handleText(change:)),
+				name: UITextView.textDidChangeNotification,
+				object: nil
+		)
+	}
+	
+	@objc func handleText(change notification: Notification) {
+		updateScrollAvailability()
+	}
+	
+}
---
+++ ZenUIKit/SourceCode/Extensions/Views/TextView/AutosizingTextView/AutosizingTextViewDelegate.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public protocol AutosizingTextViewDelegate: AnyObject {
+	
+	func autosizingTextViewDidUpdateLayout(textView: AutosizingTextView)
+	
+}
