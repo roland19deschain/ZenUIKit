1bf1f17b5df88332e3da3559a645ac1e0f53c786
Alexey Roik <alexey.roik@nebulagames.org>
2020-10-11 21:55:27 +0300

    Add 'UIBezierPath+Arrow' extension

---
+++ ZenUIKit/SourceCode/Extensions/UIKit/BezierPath/UIBezierPath+Arrow.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+public extension UIBezierPath {
+	
+	/**
+	Adds an arrow to the path pointing from the start point to the end point.
+	- Parameter start: Arrow start point.
+	- Parameter end: Arrow end point.
+	- Parameter lineLength: Length of pointer lines.
+	- Parameter angle: Фrrow angle.
+	*/
+	func addArrow(
+		start: CGPoint,
+		end: CGPoint,
+		lineLength: CGFloat,
+		angle: CGFloat
+	) {
+		move(to: start)
+		addLine(to: end)
+		
+		let startEndAngle: CGFloat = atan((end.y - start.y) / (end.x - start.x))
+			+ ((end.x - start.x) < 0 ? .pi : 0)
+		
+		let firstLineEnd = CGPoint(
+			x: end.x + lineLength * cos(.pi - startEndAngle + angle),
+			y: end.y - lineLength * sin(.pi - startEndAngle + angle)
+		)
+		let secondLineEnd = CGPoint(
+			x: end.x + lineLength * cos(.pi - startEndAngle - angle),
+			y: end.y - lineLength * sin(.pi - startEndAngle - angle)
+		)
+		
+		addLine(to: firstLineEnd)
+		move(to: end)
+		addLine(to: secondLineEnd)
+	}
+	
+}
