18cad94c56a73097175e482e359c0214f8f32685
Alexey Roik <alexey.roik@nebulagames.org>
2021-05-31 00:18:39 +0300

    Add 'UIDevice' extension

---
+++ ZenUIKit/SourceCode/Extensions/UIKit/Device/UIDevice+Convenience.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+public extension UIDevice {
+	
+	/// Returns true if the device is a iPad.
+	static var isPad: Bool {
+		current.userInterfaceIdiom == .pad
+	}
+	
+	/// Returns true if the device is a iPhone.
+	static var isPhone: Bool {
+		current.userInterfaceIdiom == .phone
+	}
+	
+	/// Returns true if the device is a Apple TV.
+	static var isTV: Bool {
+		current.userInterfaceIdiom == .tv
+	}
+	
+	/// Returns true if the device is a Mac.
+	static var isMac: Bool {
+		if #available(iOS 14.0, *) {
+			return current.userInterfaceIdiom == .mac
+		} else {
+			return false
+		}
+	}
+	
+	/// Returns true if the device is a vehicle with CarPlay.
+	static var isCarPlay: Bool {
+		current.userInterfaceIdiom == .carPlay
+	}
+	
+}
