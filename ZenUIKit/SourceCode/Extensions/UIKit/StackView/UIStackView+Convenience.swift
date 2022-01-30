4337da868cb1f30d46628ce1254a8886f70b83c9
roland19deschain <alexey.roik@nebulagames.org>
2022-01-31 02:20:56 +0300

    Update 'UIStackView+Convenience' extensions

--- ZenUIKit/SourceCode/Extensions/UIKit/StackView/UIStackView+Convenience.swift
+++ ZenUIKit/SourceCode/Extensions/UIKit/StackView/UIStackView+Convenience.swift
@@ -2,6 +2,14 @@ import UIKit
 
 public extension UIStackView {
 	
+	/// Adds a views to the end of the arrangedSubviews array,
+	/// also automatically adds each of the provided views as a subview of the stack view, if it is not already.
+	func addArrangedSubviews(_ arrangedSubviews: [UIView]) {
+		arrangedSubviews.forEach {
+			addArrangedSubview($0)
+		}
+	}
+	
 	/// Removes a subview from the list of arranged subviews,
 	/// also removing it as a subview of the receiver.
 	func removeAllArrangedSubviews() {
