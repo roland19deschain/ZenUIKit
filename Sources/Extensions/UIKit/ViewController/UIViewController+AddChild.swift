7a5c23de3143e132cb069bd0960fbd910a24abaf
roland19deschain <roland19deschain@gmail.com>
2022-10-10 01:27:37 +0300

    Update 'UIViewController' extensions, update version to '2.0.20'

--- Sources/Extensions/UIKit/ViewController/UIViewController+AddChild.swift
+++ Sources/Extensions/UIKit/ViewController/UIViewController+AddChild.swift
@@ -6,49 +6,100 @@ public extension UIViewController {
 	 Adds the specified view controller as a child of the receiver,
 	 and adds the specified view controller's view to the end of
 	 the specified view list of subviews.
-	 - parameter childViewController: The view controller to be added as a child.
-	 - parameter view: The view on which _childViewController.view_ will be placed.
+	 - parameter viewController: The view controller to be added as a child.
+	 - parameter targetView: The view on which `viewController.view` will be placed.
 	 */
-	func add(
-		_ childViewController: UIViewController,
-		to view: UIView
+	func addChild(
+		viewController: UIViewController,
+		targetView: UIView
 	) {
-		addChild(childViewController)
-		childViewController.didMove(toParent: self)
-		childViewController.view.frame = view.bounds
-		view.addSubview(childViewController.view)
+		addChild(viewController)
+		viewController.didMove(toParent: self)
+		viewController.view.frame = targetView.bounds
 		
-		childViewController
+		viewController
 			.view
 			.translatesAutoresizingMaskIntoConstraints = false
 		
-		NSLayoutConstraint.activate([
-			childViewController.view.leadingAnchor.constraint(
-				equalTo: view.leadingAnchor
-			),
-			childViewController.view.trailingAnchor.constraint(
-				equalTo: view.trailingAnchor
-			),
-			childViewController.view.topAnchor.constraint(
-				equalTo: view.topAnchor
-			),
-			childViewController.view.bottomAnchor.constraint(
-				equalTo: view.bottomAnchor
-			)
-		])
+		targetView.addSubview(viewController.view) {
+			viewController.view.boundsConstraints(to: targetView)
+		}
+	}
+	
+	/**
+	 Adds the specified view controller as a child of the receiver,
+	 and adds the specified view controller's view to the end of
+	 the specified view list of subviews.
+	 - parameter viewController: The view controller to be added as a child.
+	 - parameter view: The view on which `viewController.view` will be placed.
+	 - parameter diration: The total duration of the animations, measured in seconds.
+	 - parameter delay: The amount of time (measured in seconds) to wait before beginning the animations.
+	 - parameter completion: A block object to be executed when the animation sequence ends.
+	 */
+	func addChildAnimated(
+		viewController: UIViewController,
+		targetView: UIView,
+		duration: TimeInterval,
+		delay: TimeInterval,
+		completion: ((Bool) -> Void)? = nil
+	) {
+		viewController.view.alpha = 0
+		
+		addChild(
+			viewController: viewController,
+			targetView: targetView
+		)
+		
+		UIView.animate(
+			withDuration: duration,
+			delay: delay,
+			options: .curveEaseInOut,
+			animations: {
+				viewController.view.alpha = 1
+			},
+			completion: completion
+		)
 	}
 	
 	/**
 	 Removes the view controller from its parent, also unlinks the
 	 specified view controller's view from its superview and its window,
 	 and removes it from the responder chain.
-	 - parameter childViewController: The view controller to be removed from parent.
+	 - parameter viewController: The view controller to be removed from parent.
 	 */
-	func remove(_ childViewController: UIViewController) {
-		childViewController.willMove(toParent: nil)
-		childViewController.view.removeFromSuperview()
-		childViewController.removeFromParent()
+	func removeChild(viewController: UIViewController) {
+		viewController.willMove(toParent: nil)
+		viewController.view.removeFromSuperview()
+		viewController.removeFromParent()
+	}
+	
+	/**
+	 Removes the view controller from its parent, also unlinks the
+	 specified view controller's view from its superview and its window,
+	 and removes it from the responder chain.
+	 - parameter viewController: The view controller to be removed from parent.
+	 - parameter diration: The total duration of the animations, measured in seconds.
+	 - parameter delay: The amount of time (measured in seconds) to wait before beginning the animations.
+	 - parameter completion: A block object to be executed when the animation sequence ends.
+	 */
+	func removeChildAnimated(
+		viewController: UIViewController,
+		duration: TimeInterval,
+		delay: TimeInterval,
+		completion: ((Bool) -> Void)? = nil
+	) {
+		UIView.animate(
+			withDuration: duration,
+			delay: delay,
+			options: .curveEaseInOut,
+			animations: {
+				viewController.view.alpha = 0
+			},
+			completion: { success in
+				self.removeChild(viewController: viewController)
+				completion?(success)
+			}
+		)
 	}
 	
 }
-
