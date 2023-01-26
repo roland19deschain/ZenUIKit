eda49019398ace706125b85aed48eaeedea60a88
roland19deschain <roland19deschain@gmail.com>
2023-01-27 02:47:40 +0300

    Update 'UINavigationController+Convenience' extensions

--- Sources/Extensions/UIKit/NavigationController/UINavigationController+Convenience.swift
+++ Sources/Extensions/UIKit/NavigationController/UINavigationController+Convenience.swift
@@ -7,7 +7,7 @@ public extension UINavigationController {
 	- parameter viewController: The view controller to push onto the stack.
 	This object cannot be a tab bar controller. If the view controller is already on the navigation stack,
 	this method throws an exception.
-	- parameter animated: Specify _ true_ to animate the transition or _false_ if you do not want
+	- parameter animated: Specify _true_ to animate the transition or _false_ if you do not want
 	the transition to be animated. You might specify _false_ if you are setting up the navigation controller at launch time.
 	- parameter completion: The closure to be executed at the end of the animation.
 	*/
@@ -30,6 +30,26 @@ public extension UINavigationController {
 		coordinator.animate(alongsideTransition: nil) { _ in
 			completion()
 		}
+	}
+	
+	/**
+	 Pops the top view controller from the navigation stack and updates the display.
+	- parameter viewController: The view controller to push onto the stack.
+	This object cannot be a tab bar controller. If the view controller is already on the navigation stack,
+	this method throws an exception.
+	- parameter animated: Set this value to _true_ to animate the transition.
+	 Pass _false_ if you are setting up a navigation controller before its view is displayed.
+	- parameter completion: The closure to be executed at the end of the animation.
+	*/
+	func popViewController(
+		animated: Bool,
+		completion: @escaping () -> Void
+	) -> UIViewController? {
+		CATransaction.begin()
+		CATransaction.setCompletionBlock(completion)
+		let viewController = popViewController(animated: animated)
+		CATransaction.commit()
+		return viewController
 	}
 	
 }
