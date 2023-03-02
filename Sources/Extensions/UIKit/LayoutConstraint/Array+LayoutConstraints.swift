a3cdee77aa8f778ddbec4e1419fc808e81e50a1c
roland19deschain <roland19deschain@gmail.com>
2023-03-02 23:38:01 +0300

    Update 'Array+LayoutConstraints'

--- Sources/Extensions/UIKit/LayoutConstraint/Array+LayoutConstraints.swift
+++ Sources/Extensions/UIKit/LayoutConstraint/Array+LayoutConstraints.swift
@@ -7,4 +7,24 @@ public extension Array where Element: NSLayoutConstraint {
 		NSLayoutConstraint.activate(self)
 	}
 	
+	/// Returns a new array with prioritized constraints.
+	///
+	/// - Parameter priority: The priority to be set.
+	/// - Returns: A new array with prioritized constraints.
+	func priority(_ priority: Float) -> [NSLayoutConstraint] {
+		map {
+			$0.priority(priority)
+		}
+	}
+	
+	/// Returns a new array with prioritized constraints.
+	///
+	/// - Parameter priority: The priority to be set.
+	/// - Returns: A new array with prioritized constraints.
+	func priority(_ priority: UILayoutPriority) -> [NSLayoutConstraint] {
+		map {
+			$0.priority(priority)
+		}
+	}
+	
 }
