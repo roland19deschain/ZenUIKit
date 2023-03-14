a5cea6ae4c1ea39a1631febf378218d3bf82b7b0
roland19deschain <roland19deschain@gmail.com>
2023-03-14 18:56:37 +0300

    Update 'UINavigationController' extensions, update version to '2.0.37'

--- Sources/Extensions/UIKit/NavigationController/UINavigationController+Convenience.swift
+++ Sources/Extensions/UIKit/NavigationController/UINavigationController+Convenience.swift
@@ -2,15 +2,15 @@ import UIKit
 
 public extension UINavigationController {
 	
-	/**
-	Pushes a view controller onto the receiver’s stack and updates the display.
-	- parameter viewController: The view controller to push onto the stack.
-	This object cannot be a tab bar controller. If the view controller is already on the navigation stack,
-	this method throws an exception.
-	- parameter animated: Specify _true_ to animate the transition or _false_ if you do not want
-	the transition to be animated. You might specify _false_ if you are setting up the navigation controller at launch time.
-	- parameter completion: The closure to be executed at the end of the animation.
-	*/
+	/// Pushes a view controller onto the receiver’s stack and updates the display.
+	/// - Parameters:
+	///   - viewController: The view controller to push onto the stack.
+	///   This object cannot be a tab bar controller. If the view controller is already on the navigation stack,
+	///   this method throws an exception.
+	///   - animated: Specify _true_ to animate the transition or _false_ if you do not want
+	///   the transition to be animated. You might specify _false_ if you are
+	///   setting up the navigation controller at launch time.
+	///   - completion: The closure to be executed at the end of the animation.
 	func push(
 		viewController: UIViewController,
 		animated: Bool,
@@ -20,7 +20,6 @@ public extension UINavigationController {
 			viewController,
 			animated: animated
 		)
-		
 		guard animated, let coordinator = transitionCoordinator else {
 			DispatchQueue.main.async {
 				completion()
@@ -32,16 +31,12 @@ public extension UINavigationController {
 		}
 	}
 	
-	/**
-	 Pops the top view controller from the navigation stack and updates the display.
-	- parameter viewController: The view controller to push onto the stack.
-	This object cannot be a tab bar controller. If the view controller is already on the navigation stack,
-	this method throws an exception.
-	- parameter animated: Set this value to _true_ to animate the transition.
-	 Pass _false_ if you are setting up a navigation controller before its view is displayed.
-	- parameter completion: The closure to be executed at the end of the animation.
-	*/
-	func popViewController(
+	/// Pops the top view controller from the navigation stack and updates the display.
+	/// - Parameters:
+	///   - animated: Set this value to _true_ to animate the transition.
+	///   - completion: The closure to be executed at the end of the animation.
+	/// - Returns: The view controller that was popped from the stack.
+	@discardableResult func popViewController(
 		animated: Bool,
 		completion: @escaping () -> Void
 	) -> UIViewController? {
