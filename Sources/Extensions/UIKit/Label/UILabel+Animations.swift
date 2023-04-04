90f16916591e27a3a98fb85a71cded1f9902e084
roland19deschain <roland19deschain@gmail.com>
2023-04-04 18:34:03 +0300

    Update 'UILabel' extensions

--- Sources/Extensions/UIKit/Label/UILabel+Animations.swift
+++ Sources/Extensions/UIKit/Label/UILabel+Animations.swift
@@ -2,18 +2,67 @@ import UIKit
 
 public extension UILabel {
 	
+	/// Creates a transition animation from receivers text color to the specified new one.
+	/// - Parameters:
+	///   - duration: The total duration of the animations, measured in seconds.
+	///   - delay: The amount of time (measured in seconds) to wait before beginning the animations.
+	///   - newColor: New text color value.
+	///   - completion: A block object to be executed when the animation sequence ends.
 	func animateTextColor(
 		duration: TimeInterval = 0.3,
 		delay: TimeInterval = 0,
 		newColor: UIColor,
 		completion: ((Bool) -> Void)? = nil
 	) {
-		DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+		if delay > 0 {
+			DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+				UIView.transition(
+					with: self,
+					duration: duration,
+					options: .transitionCrossDissolve,
+					animations: { self.textColor = newColor },
+					completion: completion
+				)
+			}
+		} else {
 			UIView.transition(
 				with: self,
 				duration: duration,
 				options: .transitionCrossDissolve,
 				animations: { self.textColor = newColor },
+				completion: completion
+			)
+		}
+	}
+	
+	/// Creates a transition animation from receivers text to the specified new one.
+	/// - Parameters:
+	///   - duration: The total duration of the animations, measured in seconds.
+	///   - delay: The amount of time (measured in seconds) to wait before beginning the animations.
+	///   - newText: New text value.
+	///   - completion: A block object to be executed when the animation sequence ends.
+	func animateTextChange(
+		duration: TimeInterval = 0.3,
+		delay: TimeInterval = 0,
+		newText: String,
+		completion: ((Bool) -> Void)? = nil
+	) {
+		if delay > 0 {
+			DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+				UIView.transition(
+					with: self,
+					duration: duration,
+					options: .transitionCrossDissolve,
+					animations: { self.text = newText },
+					completion: completion
+				)
+			}
+		} else {
+			UIView.transition(
+				with: self,
+				duration: duration,
+				options: .transitionCrossDissolve,
+				animations: { self.text = newText },
 				completion: completion
 			)
 		}
--- Sources/Extensions/UIKit/Label/UILabel+Convenience.swift
+++ Sources/Extensions/UIKit/Label/UILabel+Convenience.swift
@@ -4,9 +4,11 @@ public extension UILabel {
 	
 	/// A Boolean value indicating whether the part of text is hidden.
 	var isTruncated: Bool {
-		guard let font = font,
-			let size = text?.size(withAttributes: [.font: font]) else {
-				return false
+		guard
+			let font,
+			let size = text?.size(withAttributes: [.font: font])
+		else {
+			return false
 		}
 		return size.width > bounds.size.width
 	}
